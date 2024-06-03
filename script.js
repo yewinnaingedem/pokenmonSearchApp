41168b3bcf76c15fc946e7d78b2d0747f8393ad0
yewinnaingedem <yewinnaing11605@gmail.com>
2024-06-03 04:59:53 -0700

    feat: find your pokenmon

--- script.js
+++ script.js
@@ -8,17 +8,28 @@ const poheight = document.getElementById('height');
 const imgContainer = document.getElementById('img-container');
 const types = document.getElementById("types")
 
+// resultApiCall
+searchBtn.addEventListener('click' ,resultApiCall );
+// () => {
+    // let formattedValue =  searchInput.value.toLowerCase().replace(/[^\sa-zA-Z0-9]/g , ''); 
+    // let resultValue ;
+    // if (/^[a-zA-Z]+$/.test(formattedValue)) {
+    //     resultValue = formattedValue ;
+    // }else {
+    //     resultValue = formattedValue.split(" ");
+    // }
+    // console.log(resultValue);
+    // }
 
-searchBtn.addEventListener('click' ,resultApiCall);
 
-async function resultApiCall (res) {
+async function resultApiCall () {
     let formattedValue =  searchInput.value.toLowerCase().replace(/[^\sa-zA-Z0-9]/g , '');
     const data = await fetch(`${url}/${formattedValue}`)
     .then((res) => res.json())
     .catch(err => {
         alert("Pokémon not found");
     });
-    const {name , height , id ,weight , sprites , base_experience , stats } = data ;
+    const {name , height , id ,weight , sprites ,types , base_experience , stats } = data ;
     pokemonName.textContent = name ;
     pokemonId.textContent = id ;
     poheight.textContent = height ;
@@ -30,9 +41,19 @@ async function resultApiCall (res) {
         document.getElementById(`${name}`).innerText  = base_stat ;
         
     });
-    const {front_default , back_shiny_female , front_female , front_shiny_female , back_shiny} = sprites ;
+    const {front_default} = sprites ;
+    if(document.getElementById('sprite')) {
+        document.getElementById('img-container').removeChild(document.getElementById('sprite')) ;
+    }
     const img = document.createElement('img')
     img.src = front_default ;
     img.id = 'sprite' ;
     imgContainer.appendChild(img) ;
+    let nameContainer = ""
+    types.forEach(el => {
+        const {slot , type} = el ;
+        const {name , url } = type ;
+        nameContainer += `<button class="btn btn-primary">${name.toUpperCase()}</button>` ;
+    });
+    document.getElementById('types').innerHTML  = nameContainer ;
 }   
